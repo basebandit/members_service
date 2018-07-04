7f73cf6cf8b01093f4e7b6139a0ed3b773d25fd4
basebandit <evansonmwangi83@gmail.com>
2018-07-04 04:39:36 +0300

    service resolvers

---
+++ resolvers.js
@@ -0,0 +1,34 @@
+import { promisify } from "util"
+
+export default async ({ hemera, pInsert, pool, redisClient }) => {
+  const { log } = hemera
+  //Lets promisify redis
+  let hset = promisify(redisClient.hset).bind(redisClient)
+  let hget = promisify(redisClient.hget).bind(redisClient)
+
+  const create_user = async ({ address, fname, mname, lname }) => {
+    const timestamp = Math.floor(Date.now() / 1000) //current timestamp or epoch time or Unix timestamp
+    //in seconds (Total seconds since 1970/01/01)
+
+    //insert into postgres
+    await pool.query("INSERT INTO members.registered_members VALUES ($1,$2)", [
+      address,
+      fname,
+      mname,
+      lname
+    ])
+
+    //insert into redis hash
+    await hset("registered-members", address, timestamp)
+
+    return timestamp //time user was registered
+  }
+  //fetch user address and timestamp from redis hash set
+  const fetch_user = async ({ address }) =>
+    await hget("registered-members", address)
+
+  return {
+    create_user,
+    fetch_user
+  }
+}
